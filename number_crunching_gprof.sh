f54d806414394b4da5f3be38afd0de07625c7fb7
xiaoqian CHENG <vfph55@durham.ac.uk>
2025-04-21 11:39:19 +0800

    c

---
+++ number_crunching_gprof.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH -p shared
+
+module load gcc
+module load gprof
+
+g++ -pg -o number_crunching number_crunching.cpp
+
+mkdir -p gprof_results
+for k in {1..10}
+do
+    N=$((k * 10000))
+    echo "Running for N = $N"
+    ./number_crunching $N
+    gprof number_crunching gmon.out > gprof_results_k${k}.txt
+done
+# the largest input data set
+cp gprof_results_k10.txt number_crunching_gprof.out
+
