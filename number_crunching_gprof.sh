e977edaff8e0881a3757bcb031bc71201e660d4b
vfph55 <vfph55@durham.ac.uk>
2025-04-22 10:57:16 +0100

    3

--- number_crunching_gprof.sh
+++ number_crunching_gprof.sh
@@ -17,10 +17,3 @@ done
 # the largest input data set
 cp gprof_results/gprof_results_k10.txt number_crunching_gprof.out
 
-echo "N,Time(s)" > execution_times.csv
-for k in {1..10}; 
-do
-    N=$((k * 10000))
-    time=$(grep "main" gprof_results/gprof_results_k${k}.txt | awk '{print $6}')
-    echo "$N,$time" >> execution_times.csv
-done
