a6f3383693ba8fc574b061bf62fcc976f4b200fd
vfph55 <vfph55@durham.ac.uk>
2025-04-22 11:16:12 +0100

    likwid

--- number_crunching_likwid.sh
+++ number_crunching_likwid.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+#SBATCH -p test
+#SBATCH --exclusive
 
-g++ -O3 -DLIKWID_PERFMON number_crunching_likwid.cpp -o number_crunching_likwid -llikwid
-
-likwid-perfctr -C 0 -g MEMORY,FLOPS_DP ./number_crunching_likwid 10000
+g++ -O3 -DLIKWID_PERFMON -I${LIKWID_INCLUDE} -L${LIKWID_LIB} -llikwid number_crunching_likwid.cpp -o number_crunching_likwid
+likwid-perfctr -C 0 -g MEMORY,FLOPS_DP ./number_crunching_likwid 10000 > number_crunching_likwid.out
