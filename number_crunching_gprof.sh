9a6256f35282ab714495375050525628150e1127
vfph55 <vfph55@durham.ac.uk>
2025-04-24 18:28:30 +0100

    4

--- number_crunching_gprof.sh
+++ number_crunching_gprof.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
-#SBATCH -p shared
+#SBATCH -p test
+#SBATCH --exclusive
 
 module load gcc
 
-g++ -pg -O3 number_crunching.cpp -o number_crunching
+#g++ -pg -O3 number_crunching.cpp -o number_crunching
+g++ -pg -O2 number_crunching.cpp -o number_crunching
 
 mkdir -p gprof_results
-for k in {1..10};
-do
-    N=$((k * 10000))
-    echo "Running for N = $N"
-    ./number_crunching $N
-    gprof number_crunching gmon.out > gprof_results/gprof_results_k${k}.txt
-done
+#for k in {4..5};
+#do
+#    N=$((k * 10000))
+#    echo "Running for N = $N"
+#    ./number_crunching $N
+#    gprof number_crunching gmon.out > gprof_results/gprof_results_k${k}.txt
+#done
+
+k=4
+N=$((k * 10000))
+echo "Running for N = $N"
+./number_crunching $N
+gprof number_crunching gmon.out > gprof_results/gprof_results_k${k}.txt
+
+
 # the largest input data set
 cp gprof_results/gprof_results_k10.txt number_crunching_gprof.out
 
--- number_crunching_likwid.sh
+++ number_crunching_likwid.sh
@@ -2,5 +2,8 @@
 #SBATCH -p test
 #SBATCH --exclusive
 
-g++ -O3 -DLIKWID_PERFMON -I${LIKWID_INCLUDE} -L${LIKWID_LIB} -llikwid number_crunching_likwid.cpp -o number_crunching_likwid
-likwid-perfctr -C 0 -g MEMORY,FLOPS_DP ./number_crunching_likwid 10000 > number_crunching_likwid.out
+module load likwid
+
+g++ -O3 number_crunching_likwid.cpp -o number_crunching_likwid -llikwid
+likwid-perfctr -g MEMORY,FLOPS_DP ./number_crunching_likwid 10000 > number_crunching_likwid.out
+
