9a582940189e95c74bde0fe18a4de877e0dfe06a
xiaoqian CHENG <vfph55@durham.ac.uk>
2025-04-25 05:45:03 +0800

    number_crunching_gprof.sh

--- number_crunching_gprof.sh
+++ number_crunching_gprof.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #SBATCH -p test
-#SBATCH --exclusive
 
 module load gcc
 
@@ -8,20 +7,13 @@ module load gcc
 g++ -pg -O2 number_crunching.cpp -o number_crunching
 
 mkdir -p gprof_results
-#for k in {4..5};
-#do
-#    N=$((k * 10000))
-#    echo "Running for N = $N"
-#    ./number_crunching $N
-#    gprof number_crunching gmon.out > gprof_results/gprof_results_k${k}.txt
-#done
-
-k=4
-N=$((k * 10000))
-echo "Running for N = $N"
-./number_crunching $N
-gprof number_crunching gmon.out > gprof_results/gprof_results_k${k}.txt
-
+for k in {1..9};
+do
+   N=$((k * 10000))
+   echo "Running for N = $N"
+   ./number_crunching $N
+   gprof number_crunching gmon.out > gprof_results/gprof_results_k${k}.txt
+done
 
 # the largest input data set
 cp gprof_results/gprof_results_k10.txt number_crunching_gprof.out
