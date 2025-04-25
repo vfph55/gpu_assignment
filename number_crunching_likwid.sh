22b2916edec1a3f20eb321093f42ef1044e87aa2
vfph55 <vfph55@durham.ac.uk>
2025-04-25 11:45:18 +0100

    number_crunching_likwid.sh

--- number_crunching_likwid.sh
+++ number_crunching_likwid.sh
@@ -9,12 +9,12 @@ g++ -O3 -DLIKWID_PERFMON number_crunching_likwid.cpp -o number_crunching_likwid 
 > number_crunching_likwid.out
 
 echo " floating point performance:" >> number_crunching_likwid.out
-likwid-perfctr -g FLOPS_DP ./number_crunching_likwid 10000 >> number_crunching_likwid.out
+likwid-perfctr -m -g FLOPS_DP ./number_crunching_likwid 10000 >> number_crunching_likwid.out
 #likwid-perfctr -g FLOPS_DP -C 0 -m ./number_crunching_likwid 10000 2>> number_crunching_likwid.out 
 echo -e "\n" >> number_crunching_likwid.out
 
 echo " memory performance:" >> number_crunching_likwid.out
 #likwid-perfctr -g L3 ./number_crunching_likwid 10000 > l3_bandwidth.out
 #likwid-perfctr -g CACHE ./number_crunching_likwid 10000 > cache_hits.out
-likwid-perfctr -g MEM ./number_crunching_likwid 10000 >> number_crunching_likwid.out
+likwid-perfctr -m -g MEM ./number_crunching_likwid 10000 >> number_crunching_likwid.out
 #likwid-perfctr -g MEM -C 0 -m ./number_crunching_likwid 10000 2>> number_crunching_likwid.out 
